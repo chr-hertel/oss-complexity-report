aa484c481240e583d65c749c283ab87a0e94147e
Christopher Hertel <mail@christopher-hertel.de>
2020-09-05 02:20:37 +0200

    implementing real code analysis

--- src/ComplexityReport/CodeAnalyser.php
+++ src/ComplexityReport/CodeAnalyser.php
@@ -5,11 +5,34 @@ declare(strict_types=1);
 namespace App\ComplexityReport;
 
 use App\Entity\Library;
+use SebastianBergmann\PHPLOC\Analyser;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
 
 class CodeAnalyser
 {
+    private Analyser $analyser;
+    private string $repositoryPath;
+
+    public function __construct(Analyser $analyser, string $repositoryPath)
+    {
+        $this->repositoryPath = $repositoryPath;
+        $this->analyser = $analyser;
+    }
+
     public function analyse(Library $library): Analysis
     {
-        return new Analysis(12000, 23.1);
+        $localPath = $this->repositoryPath.'/'.$library->getRepositoryPath();
+        $finder = (new Finder())
+            ->in($localPath)
+            ->name('*.php');
+
+        $files = array_map(static function (SplFileInfo $file) {
+            return $file->getRealPath();
+        }, iterator_to_array($finder));
+
+        $analysis = $this->analyser->countFiles($files, false);
+
+        return new Analysis($analysis['loc'], $analysis['classCcnAvg']);
     }
 }
