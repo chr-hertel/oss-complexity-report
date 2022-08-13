21e8817d04449e48c31f12ac3416ea111e044b98
Christopher Hertel <mail@christopher-hertel.de>
2022-08-13 20:19:30 +0200

    add deployer and adopt action

--- deploy.php
+++ deploy.php
@@ -5,23 +5,22 @@ namespace Deployer;
 require 'recipe/symfony.php';
 
 // Config
-set('repository', 'git@github.com:chr-hertel/decomplex.git');
+set('repository', 'git@github.com:chr-hertel/oss-complexity-report.git');
 set('composer_options', '--no-dev --verbose --prefer-dist --classmap-authoritative --no-progress --no-interaction --no-scripts');
 set('console_options', '--no-interaction --env=prod');
 
 // Hosts
 host('decomplex.me')
     ->set('remote_user', 'deployer')
-    ->set('deploy_path', '/var/www/decomplex');
+    ->set('deploy_path', '/var/www/oss-complexity-report');
 
 // Tasks
 task('build', function () {
     cd('{{release_path}}');
-    run('npm clean-install');
-    run('npm run build');
+    run('yarn install --frozen-lockfile');
+    run('yarn build');
     run('{{bin/console}} dotenv:dump {{console_options}}');
 });
 
 after('deploy:cache:clear', 'build');
-after('deploy:cache:clear', 'database:migrate');
 after('deploy:failed', 'deploy:unlock');
