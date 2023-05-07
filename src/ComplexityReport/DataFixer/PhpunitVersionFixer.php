7f11d8a1c0387a7fc8c906c85e74766cf1cc8247
Christopher Hertel <mail@christopher-hertel.de>
2023-05-07 14:45:04 +0200

    fix code style after update

--- src/ComplexityReport/DataFixer/PhpunitVersionFixer.php
+++ src/ComplexityReport/DataFixer/PhpunitVersionFixer.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\ComplexityReport\DataFixer;
 
 use App\Entity\Tag;
-use DateTimeImmutable;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Query\Expr\Join;
@@ -28,7 +27,7 @@ final class PhpunitVersionFixer implements FixerInterface
             ->from(Tag::class, 't')
             ->join('t.library', 'l', Join::WITH, 'l.name = "phpunit/phpunit"')
             ->where('t.created = :date')
-            ->setParameter('date', new DateTimeImmutable('2006-07-04 12:01:50'), Types::DATETIME_IMMUTABLE)
+            ->setParameter('date', new \DateTimeImmutable('2006-07-04 12:01:50'), Types::DATETIME_IMMUTABLE)
             ->getQuery()
             ->execute();
     }
